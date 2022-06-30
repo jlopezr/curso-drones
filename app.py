23de0b0f02305ea1d759127b8df3fa17d2957a71
Juan López Rubio <j.lopez.r@gmail.com>
2022-06-30 10:26:31 +0200

    app

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import sys
+import math
+from PIL import Image, ImageDraw
 import pygame
 from pygame.locals import *
 
 frames = 60
+
+def deg2rad(degrees):
+    return (degrees*2*math.pi)/360
 
 def frameRate(f):
     global frames
@@ -15,6 +20,24 @@ def getKey(keyName):
     if keyInput[myKey]:
         ans = True
     return ans
+
+def draw_filled_arc(screen, color, pos, radius, start, end):
+    pil_size = radius*2
+    pil_image = Image.new("RGBA", (pil_size, pil_size))
+    pil_draw = ImageDraw.Draw(pil_image)
+
+    pil_draw.pieslice((0, 0, pil_size-1, pil_size-1), start, end, fill=color)
+
+    # - convert into PyGame image -
+    mode = pil_image.mode
+    size = pil_image.size
+    data = pil_image.tobytes()
+    image = pygame.image.fromstring(data, size, mode)
+    image_rect = image.get_rect(center=screen.get_rect().center)
+    
+    # blit image
+    screen.blit(image, image_rect)
+    
 
 def run(draw):
     global frames
